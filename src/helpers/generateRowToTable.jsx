401075f44b301af3adb497d0255fb1fee5bb7665
Alisson dahlem <alissongod95@hotmail.com>
2022-04-04 22:49:48 -0300

    corrige erro de nomes na hora de renderizar componente

--- src/helpers/generateRowToTable.jsx
+++ src/helpers/generateRowToTable.jsx
@@ -4,7 +4,6 @@ class GenerateRowToTable extends React.Component {
   render() {
     const { description, tag, method, value, exchange, exchangeRates } = this.props;
     const exchangeName = exchangeRates[exchange].name;
-    const exchangeNameFiltred = exchangeName.substring(0, exchangeName.indexOf('/'));
     const valueExchangeUsed = exchangeRates[exchange].ask
     const valueExchangeUserToNumber = parseFloat(valueExchangeUsed);
     const ValueExchangeUserFixed = valueExchangeUserToNumber.toFixed(2);
@@ -12,19 +11,16 @@ class GenerateRowToTable extends React.Component {
     const convertedValueToTwoCase = convertedValue.toFixed(2);
     const paseFloatValue = parseFloat(value);
     const valueFixed = paseFloatValue.toFixed(2);
-
     return (
       <tr>
         <td>{ description }</td>
         <td>{ tag }</td>
         <td>{ method }</td>
         <td>{ valueFixed }</td>
-        <td>{ exchangeNameFiltred }</td>
+        <td>{ exchangeName }</td>
         <td>{ ValueExchangeUserFixed }</td>
         <td>{ convertedValueToTwoCase }</td>
         <td>Real</td>
-        {/* <button type='button'>editar</button>
-        <button type='button'>excluir</button> */}
       </tr>
     );
   }
